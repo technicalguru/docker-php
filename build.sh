4e05e3a0e8e30af7a80c62e104c3f95c63e1e4b9
ralph <github@ralph-schuster.eu>
2023-04-21 12:34:00 +0000

    Fix no-cache command

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build --progress=plain -t technicalguru/php:latest .
-#docker build --no-cache -t technicalguru/rs-php:latest .
+#docker build --progress=plain --no-cache -t technicalguru/php:latest .
