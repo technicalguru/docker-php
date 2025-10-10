2fb0b6992a74e42734c90049f7d59de5775cd249
technicalguru <github@ralph-schuster.eu>
2025-10-10 12:17:44 +0200

    Build command

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 docker buildx build --progress=plain --push \
-    -t technicalguru/php:v8.3.26-apache-2.4.65.0 \
+    -t technicalguru/php:8.3.26-apache-2.4.65.0 \
+    -t technicalguru/php:8.3-apache-2.4.65.0 \
+    -t technicalguru/php:8-apache-2.4.65.0 \
+    -t technicalguru/php:8.3.26 \
+    -t technicalguru/php:8.3 \
+    -t technicalguru/php:8 \
     --platform linux/arm64,linux/amd64 \
     .
 
